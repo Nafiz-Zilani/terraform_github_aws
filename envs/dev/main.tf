751110e213636f81c30df350bb35202316e2a3c2
Nafiz Zilani <nafiz08b@gmail.com>
2024-10-21 23:31:35 +0600

    add

---
+++ envs/dev/main.tf
@@ -0,0 +1,24 @@
+terraform { 
+  cloud { 
+    
+    organization = "poridh_iac" 
+
+    workspaces { 
+      name = "ghost-dev" 
+    } 
+  } 
+}
+
+module "poiridhi_dev" {
+  source = "terr_c_5/src/blueprint"
+  vpc_name = "poiridhi_dev_vpc"
+  vpc_cidr = "10.11.0.0/16"
+  public_subnets = ["10.11.1.0/24"]
+  availability_zones = ["ap-southeast-1a"]
+  custom_tags = {
+    env = "poridhi_dev"
+    team = "app"
+    manage_by_tf = true
+    workspaces = "ghost-dev"
+  }
+}
