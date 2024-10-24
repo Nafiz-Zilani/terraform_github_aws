ee5d501e121e35af49f20c2399e6ffb5141feb53
Nafiz Zilani <nafiz08b@gmail.com>
2024-10-24 23:02:03 +0600

    V1.4

---
+++ src/envs/dev/main.tf
@@ -0,0 +1,24 @@
+terraform { 
+  cloud { 
+    
+    organization = "poridh_iac" 
+
+    workspaces { 
+      name = "ghost-dev" 
+    } 
+  } 
+}
+
+module "poiridhi_dev" {
+  source = "/Users/nafiz/Code/VS_Code/terr_C_5/src/blueprint"
+  vpc_name = "poiridhi_dev_vpc"
+  vpc_cidr = "10.11.0.0/16"
+  public_subnets = ["10.11.1.0/24"]
+  availability_zones = ["us-east-1"]
+  custom_tags = {
+    env = "poridhi_dev"
+    team = "app"
+    manage_by_tf = true
+    workspaces = "ghost-dev"
+  }
+} 
